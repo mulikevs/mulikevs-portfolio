916190a000d7288f039d27a1b805ef0a0d6000d0
Kelvin M. Mwau [ICT] <kmwau@CO-OPBANK.CO.KE>
2024-01-16 14:53:44 +0300

    updated Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Brands from '../components/Brands';
 import Projects from '../components/Projects';
 import Testimonial from '../components/Testimonial';
